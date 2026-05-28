02b296a303b6ae2b95ce07c340c36bed0863cddc
PET EEL UFSC <50997744+peteel@users.noreply.github.com>
2026-05-28 20:53:11 -0300

    exercicio01

---
+++ aula2/exercicio01/solucao.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void caixa_registradora() {
+    int preco1, preco2;
+ 
+    printf("Digite o preco do produto 1 (em centavos): ");
+    scanf("%d", &preco1);
+    printf("Digite o preco do produto 2 (em centavos): ");
+    scanf("%d", &preco2);
+ 
+    printf("\nSoma: %d centavos\n",         preco1 + preco2);
+    printf("Subtracao: %d centavos\n",      preco1 - preco2);
+    printf("Multiplicacao: %d\n",           preco1 * preco2);
+    printf("Divisao inteira: %d\n",         preco1 / preco2);
+    printf("Resto: %d\n",                   preco1 % preco2);
+}
